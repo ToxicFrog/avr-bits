4eba62080f50b834b2f0c28ed94393c1b2d0a106
Ben Kelly <bk@ancilla.ca>
2019-02-25 22:15:14 -0500

    quiet down morningtime.py

--- morningtime.py
+++ morningtime.py
@@ -11,12 +11,12 @@ dev = usb.core.find(idVendor=0x16C0, idProduct=0x05DC)
 if dev is None:
   raise ValueError("Couldn't find the Digispark.")
 
-for cfg in dev:
-  print('cfg:', cfg.bConfigurationValue)
-  for intf in cfg:
-    print('  if:', intf.bInterfaceNumber, intf.bAlternateSetting)
-    for ep in intf:
-      print('    ep:', ep.bEndpointAddress)
+# for cfg in dev:
+#   print('cfg:', cfg.bConfigurationValue)
+#   for intf in cfg:
+#     print('  if:', intf.bInterfaceNumber, intf.bAlternateSetting)
+#     for ep in intf:
+#       print('    ep:', ep.bEndpointAddress)
 
 if sys.argv[1] == "on":
   dev.ctrl_transfer(0x40, 1, 0, 0, None)
